e6759c901ff7cf9b54ea2dd5dcea6a19378dbb65
lukew96-beep <luke.sageknight@gmail.com>
2021-12-28 13:49:33 -0600

    final touches

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
+
 console.log("Let's get this party started!");
-let APIKEY = "Y0PiOgIXIjvlHMP5Q1eqifEtt7ESd8X0";
-async function getGif(){
-    const input = getElementById("#search");
-    const search = await 
-    axios.get(`api.giphy.com/v1/gifs/search?
-    api_key=${APIKEY}&limit=1&q=${input.value}`);
-    console.log(search);
-}
-const btn = document.getElementById("#searchBtn");
-    btn.addEventListener('click', function(e){
-        e.preventDefault;
-        getGif();
-    });
+const $gifArea = $("#gif-area");
+const $searchInput = $("#search");
+
+function addGif(res) {
+    let numResults = res.data.length;
+    if (numResults) {
+      let randomIdx = Math.floor(Math.random() * numResults);
+      let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
+      let $newGif = $("<img>", {
+        src: res.data[randomIdx].images.original.url,
+        class: "w-100"
+      });
+      $newCol.append($newGif);
+      $gifArea.append($newCol);
+    }
+  }
+
+  $("form").on("submit", async function(evt) {
+    evt.preventDefault();
+
+    let searchTerm = $searchInput.val();
+    $searchInput.val("");
+  
+
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        params: {
+          q: searchTerm,
+              api_key: "Y0PiOgIXIjvlHMP5Q1eqifEtt7ESd8X0"
+          }
+      });
+      addGif(response.data);
+  });
+  $("#remove").on("click", function() {
+    $gifArea.empty();
+  });
